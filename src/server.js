29ff4f0d5449179333de64c4fba81b63799a543a
aispark <aispark@paran.com>
2019-10-11 08:16:49 +0900

    3-3 sendMail Function with Nodemailer

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { GraphQLServer } from "graphql-yoga";
-import logger from 'morgan'
-import schema from './schema'
+import logger from "morgan";
+import schema from "./schema";
+import { sendSecretMail } from "./utils";
 
+sendSecretMail('aispark@paran.com', 'aaaaa')
 const PORT = process.env.PORT || 4000;
-
-
 
 const server = new GraphQLServer({ schema });
 
-server.express.use(logger('dev'))
+server.express.use(logger("dev"));
 
-server.start({ port: PORT }, () => console.log(`Server running on port http://localhost:${PORT}`));
+server.start({ port: PORT }, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
