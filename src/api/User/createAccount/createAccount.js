23637f3c4e04901fc6e614e87d62c5dfe6ad8462
aispark <aispark@paran.com>
2019-10-23 20:03:50 +0900

    5-8 로그인 에러 메세지

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,12 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({
+        OR: [{ username }, { email }]
+      });
+      if (exists) {
+        throw new Error("This username / email is already taken");
+      }
       const user = await prisma.createUser({
         username,
         email,
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,16 +4,14 @@ import { generateSecret, sendSecretMail } from "../../../utils";
 export default {
   Mutation: {
     requestSecret: async (_, args) => {
-      
       const { email } = args;
-      const loginSecret = generateSecret()
+      const loginSecret = generateSecret();
       try {
-        await sendSecretMail(email, loginSecret)
+        await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
-        return true
-      } catch(error) {
-        console.log(error)
-        return false
+        return true;
+      } catch {
+        return false;
       }
     }
   }
